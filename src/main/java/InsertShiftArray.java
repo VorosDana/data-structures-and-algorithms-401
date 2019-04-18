60acdcc1b5559d85c6f936bfba4bb3200b7ed6cf
Dana Voros <vorosdana@gmail.com>
2019-04-17 17:02:48 -0700

    Adds first code challenge code

--- src/main/java/InsertShiftArray.java
+++ src/main/java/InsertShiftArray.java
@@ -1,23 +1,24 @@
 //package challenge02;
 
-public class InsertShiftArray {
-    public static void main(String[] args){
+public class InsertShiftArray
 
-    }
+        public static void main(String[] args) {
 
-    public static int[] insertShiftArray(int[] input, int newNum){
-        int[] output = new int[input.length + 1];
-
-        for(int i = 0; i < input.length - input.length / 2; i++){
-            output[i] = input[i];
         }
 
-        output[output.length/2] = newNum;
+        public static int[] insertShiftArray(int[] input, int newNum) {
+            int[] output = new int[input.length + 1];
 
-        for(int i = 0; i < input.length / 2; i++){
-            output[output.length - 1 - i] = input[input.length - 1 - i];
+            for (int i = 0; i < input.length - input.length / 2; i++) {
+                output[i] = input[i];
+            }
+
+            output[output.length / 2] = newNum;
+
+            for (int i = 0; i < input.length / 2; i++) {
+                output[output.length - 1 - i] = input[input.length - 1 - i];
+            }
+
+            return output;
         }
-
-        return output;
-    }
 }
