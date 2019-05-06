8c23a04e523ed2c58899f9ed2527b3d9effc9dd7
Dana Voros <vorosdana@gmail.com>
2019-05-06 09:40:06 -0700

    tree.BinarySearchTree.add impemented, tree.BinarySearchTree<T> moved to Integer type

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -5,47 +5,73 @@ import stacksandqueues.Queue;
 import java.util.Collection;
 import java.util.List;
 
-public class BinarySearchTree<T> {
-    private Node<T> root;
+public class BinarySearchTree {
+    private Node<Integer> root;
 
     public BinarySearchTree() {
         super();
     }
 
-    public BinarySearchTree(Collection<T> vals) {
-        java.util.Queue<T> valsQ = new java.util.PriorityQueue<>(vals);
+    public BinarySearchTree(Collection<Integer> vals) {
+        java.util.Queue<Integer> valsQ = new java.util.PriorityQueue<>(vals);
         this.root = new Node<>();
-        Queue<Node<T>> q = new Queue<>();
+        Queue<Node<Integer>> q = new Queue<>();
         q.enqueue(this.root);
 
         while (!valsQ.isEmpty()) {
-            Node<T> current = q.dequeue();
+            Node<Integer> current = q.dequeue();
             current.setVal(valsQ.remove());
-            current.setLeft(new Node<T>());
-            current.setRight(new Node<T>());
+            current.setLeft(new Node<Integer>());
+            current.setRight(new Node<Integer>());
             q.enqueue(current.getLeft());
             q.enqueue(current.getRight());
         }
     }
 
-    public void add(T val) {
+    public void add(Integer val) {
+        if (this.root == null) {
+            this.root = new Node<Integer>();
+            this.root.setVal(val);
+        }
 
+        Node<Integer> current = this.root;
+
+        while (true) {
+            if (val >= current.getVal()) {
+                if (current.getRight() != null) {
+                    current = current.getRight();
+                } else {
+                    Node<Integer> valNode = new Node<>();
+                    valNode.setVal(val);
+                    current.setRight(valNode);
+                }
+            } else {
+                if (current.getLeft() != null) {
+                    current = current.getLeft();
+                } else {
+                    Node<Integer> valNode = new Node<>();
+                    valNode.setVal(val);
+                    current.setLeft(valNode);
+                }
+            }
+
+        }
     }
 
-    public boolean contains(T val) {
-        Queue<Node<T>> q = new Queue<>();
+    public boolean contains(Integer val) {
+        Queue<Node<Integer>> q = new Queue<>();
         q.enqueue(this.root);
 
         while (q.peek() != null) {
-            Node<T> current = q.dequeue();
-            if (current.getVal() == val) {
+            Node<Integer> current = q.dequeue();
+            if (current.getVal().equals(val)) {
                 return true;
             }
 
             if (current.getLeft() != null) {
                 q.enqueue(current.getLeft());
             }
-            
+
             if (current.getRight() != null) {
                 q.enqueue(current.getRight());
             }
