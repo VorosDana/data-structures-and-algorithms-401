c5f688fbc2b6292af5568182a1a40475dc9e3afb
Dana Voros <vorosdana@gmail.com>
2019-05-06 09:10:10 -0700

    tree.BinarySearchTree stubbed out

---
+++ src/main/java/tree/BinarySearchTree.java
@@ -0,0 +1,13 @@
+package tree;
+
+public class BinarySearchTree<T> {
+    private Node<T> root;
+
+    public void add(T val) {
+
+    }
+
+    public boolean contains(T val) {
+        return false;
+    }
+}
