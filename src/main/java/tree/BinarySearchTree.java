2ca568554ec10567ddd0bcde524d52b352fe03b1
Dana Voros <vorosdana@gmail.com>
2019-05-06 09:23:04 -0700

    tree constructors implemented, including InOrder build from Collection

--- src/main/java/tree/BinarySearchTree.java
+++ src/main/java/tree/BinarySearchTree.java
@@ -1,7 +1,32 @@
 package tree;
 
+import stacksandqueues.Queue;
+
+import java.util.Collection;
+import java.util.List;
+
 public class BinarySearchTree<T> {
     private Node<T> root;
+
+    public BinarySearchTreeTree() {
+        super();
+    }
+
+    public BinarySearchTree(Collection<T> vals) {
+        java.util.Queue<T> valsQ = new java.util.PriorityQueue<>(vals);
+        this.root = new Node<>();
+        Queue<Node<T>> q = new Queue<>();
+        q.enqueue(this.root);
+
+        while (!valsQ.isEmpty()) {
+            Node<T> current = q.dequeue();
+            current.setVal(valsQ.remove());
+            current.setLeft(new Node<T>());
+            current.setRight(new Node<T>());
+            q.enqueue(current.getLeft());
+            q.enqueue(current.getRight());
+        }
+    }
 
     public void add(T val) {
 
--- src/main/java/tree/BinaryTree.java
+++ src/main/java/tree/BinaryTree.java
@@ -1,11 +1,33 @@
 package tree;
 
+import stacksandqueues.Queue;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class BinaryTree<T> {
     private Node<T> root;
 
+    public BinaryTree() {
+        super();
+    }
+
+    public BinaryTree(Collection<T> vals) {
+        java.util.Queue<T> valsQ = new java.util.PriorityQueue<>(vals);
+        this.root = new Node<>();
+        Queue<Node<T>> q = new Queue<>();
+        q.enqueue(this.root);
+
+        while (!valsQ.isEmpty()) {
+            Node<T> current = q.dequeue();
+            current.setVal(valsQ.remove());
+            current.setLeft(new Node<T>());
+            current.setRight(new Node<T>());
+            q.enqueue(current.getLeft());
+            q.enqueue(current.getRight());
+        }
+    }
 
     public List<T> preOrderTraversal() {
         List<T> output = new ArrayList<>();
