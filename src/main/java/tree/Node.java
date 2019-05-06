5ce93ea79291806eeda93750b40fa32873dd691e
Dana Voros <vorosdana@gmail.com>
2019-05-06 08:54:18 -0700

    tree.Node implemented

---
+++ src/main/java/tree/Node.java
@@ -0,0 +1,32 @@
+package tree;
+
+public class Node<T> {
+    private Node<T> left;
+    private Node<T> right;
+    private T val;
+
+
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
+
+    public T getVal() {
+        return val;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+}
