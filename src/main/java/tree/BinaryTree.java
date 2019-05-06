a500f2c35e715c01f5cc340330957a5586e7c9f6
Dana Voros <vorosdana@gmail.com>
2019-05-06 09:03:56 -0700

    tree.BinaryTree stubbed out

---
+++ src/main/java/tree/BinaryTree.java
@@ -0,0 +1,24 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTree<T> {
+    private Node<T> root;
+
+
+    public List<String> preOrderTraversal() {
+        List<String> output = new ArrayList<>() List<> ();
+        return output;
+    }
+
+    public List<String> inOrderTraversal() {
+        List<String> output = new ArrayList<>() List<> ();
+        return output;
+    }
+
+    public List<String> postOrderTraversal() {
+        List<String> output = new ArrayList<>() List<> ();
+        return output;
+    }
+}
