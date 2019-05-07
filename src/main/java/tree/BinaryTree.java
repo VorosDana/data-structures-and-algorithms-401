58503183482b2cf80c1a7e7ccf80725e522596d1
Dana Voros <vorosdana@gmail.com>
2019-05-07 09:59:26 -0700

    Found bugs in traversals, all were preorder

--- src/main/java/tree/BinaryTree.java
+++ src/main/java/tree/BinaryTree.java
@@ -69,10 +69,11 @@ public class BinaryTree<T> {
     }
 
     private void inOrderInternal(Node<T> current, List<T> output) {
-        output.add(current.getVal());
+
         if (current.getLeft() != null) {
             inOrderInternal(current.getLeft(), output);
         }
+        output.add(current.getVal());
         if (current.getRight() != null) {
             inOrderInternal(current.getRight(), output);
         }
@@ -88,13 +89,14 @@ public class BinaryTree<T> {
     }
 
     private void postOrderInternal(Node<T> current, List<T> output) {
-        output.add(current.getVal());
+
         if (current.getLeft() != null) {
             postOrderInternal(current.getLeft(), output);
         }
         if (current.getRight() != null) {
             postOrderInternal(current.getRight(), output);
         }
+        output.add(current.getVal());
     }
 
 }
