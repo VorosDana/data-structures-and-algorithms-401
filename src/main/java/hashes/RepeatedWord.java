691f8bca2ec367a463736a2da108493eba2dcd3d
Dana Voros <vorosdana@gmail.com>
2019-05-28 09:09:17 -0700

    Tests for repeated word implemented, also bug fixes

--- src/main/java/hashes/RepeatedWord.java
+++ src/main/java/hashes/RepeatedWord.java
@@ -7,11 +7,21 @@ public class RepeatedWord {
         HashMap<String> foundWords = new HashMap<>();
 
         for (String word : words) {
+
+            // Removes whitespace and puts word in lowercase, to
+            // remove case sensitivity
+            // https://stackoverflow.com/questions/18830813/how-can-i-remove-punctuation-from-input-text-in-java
+            word = word.replaceAll("[^A-Za-z]", "").toLowerCase();
+
+            // Add returns true if value goes if, false if not
+            // If it returns false, that word is already in,
+            // and we found our duplicate!
             if (!foundWords.add(word, word)) {
                 return word;
             }
         }
 
-        return "";
+        // iff no repeats found, return null
+        return null;
     }
 }
